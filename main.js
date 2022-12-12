b4d314c479a68159b14d7101efdec4f6c37f7da5
Will Hedges <wshedges@outlook.com>
2022-12-12 09:38:08 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+/*
+  Building an effect pedal
+*/
+
+const printDrillTemplate = () => {
+  console.log("Printing the drill template");
+};
+
+const markHoles = () => {
+  console.log("Marking the holes on the enclosure");
+};
+
+const drillEnclosure = () => {
+  console.log("Using the drill press to make holes in the enclosure");
+};
+
+const cleanEnclosure = () => {
+  console.log("Cleaning the grease and shavings off of the enclosure");
+};
+
+const printArtwork = () => {
+  console.log("Printing the waterslide decal");
+};
+
+const applyWaterslide = () => {
+  console.log("Carefully applying the decal");
+};
+
+const clearcoatEnclsoure = () => {
+  console.log("Spraying on the clear coat");
+};
+
+const solderComponents = () => {
+  console.log("Soldering the pedal circuit");
+};
+
+const assemblePedal = () => {
+  console.log("Assembling the components in the enclosure");
+};
+
+const testPedal = () => {
+  console.log("Testing that the circuit works and the LED toggles on and off");
+};
+
+const shakeHouse = () => {
+  console.log("Driving my wife and cat crazy with the power of bass");
+};
+
+printDrillTemplate();
+markHoles();
+drillEnclosure();
+cleanEnclosure();
+printArtwork();
+applyWaterslide();
+clearcoatEnclsoure();
+solderComponents();
+assemblePedal();
+testPedal();
+shakeHouse();
